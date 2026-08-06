d329f075e5d3638d90dd0cd1d81aa52eaa35b096
Anubhav <choubey.anubhav253@gmail.com>
2026-08-06 23:52:24 +0530

    Cleaning Some Stuff up in here

--- admin/src/routers/base.py
+++ admin/src/routers/base.py
@@ -1,6 +1,9 @@
-from fastapi import APIRouter, Request
+import os
 
-from core.config import templates
+from fastapi import APIRouter, Request
+from fastapi.responses import FileResponse
+
+from core.config import STATIC_DIR, templates
 
 router = APIRouter()
 
@@ -8,3 +11,8 @@ router = APIRouter()
 @router.get("/")
 def home(request: Request):
     return templates.TemplateResponse(request, "pages/landing.html")
+
+
+@router.get("/favicon.ico", include_in_schema=False)
+async def favicon():
+    return FileResponse(os.path.join(STATIC_DIR, "assets", "favicon.ico"))
